211713d70e27f428eab112379cf197922700e99c
Midesax <ayofavour202@gmail.com>
2023-04-21 06:54:38 -0700

    ..

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ printf("%d", n);
 
 if (n == 0)
 {
-    printf(" is zero\n");
+printf(" is zero\n");
 }
 else if (n > 0)
 {
-    printf(" is positive\n");
+printf(" is positive\n");
 }
 else
 {
-    printf(" is negative\n");
+printf(" is negative\n");
 }
 
 return (0);
