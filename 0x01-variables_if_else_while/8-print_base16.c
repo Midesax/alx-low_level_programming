6f1c8873669579bed8f9421a88551620731dfff5
Midesax <ayofavour202@gmail.com>
2023-04-21 13:42:15 -0700

    ...

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,8 @@ for (digit = '0'; digit <= '9'; digit++)
 {
 putchar(digit);
 }
-for (digit = 'a'; digit <= 'f'; digit++) {
+for (digit = 'a'; digit <= 'f'; digit++)
+{
 putchar(digit);
 }
 putchar('\n');
