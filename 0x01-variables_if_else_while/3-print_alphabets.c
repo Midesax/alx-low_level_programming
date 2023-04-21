e900bafcf31bbf9e8ada9571b13819667c5beff8
Midesax <ayofavour202@gmail.com>
2023-04-21 09:21:02 -0700

    ..

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/** 
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main (void)
+{
+char small_letters = 'a';
+char big_letters = 'A';
+while (small_letters <= 'z')
+{
+putchar ("small_letters");
+small_letters++;
+}
+{
+putchar ("big_letters");
+big_letters++;
+}
+putchar ("\n")
+return (0);
+}
