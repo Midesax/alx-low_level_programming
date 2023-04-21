df5c2ef3ce05f5fb6c4f9976e56fc00aaaa5dfda
Midesax <ayofavour202@gmail.com>
2023-04-21 10:54:42 -0700

    ..

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main() 
+{
+int i = 0;
+  
+while (i < 10) {
+printf("%d\n", i);
+i++;
+}
+ 
+return (0);
+}
