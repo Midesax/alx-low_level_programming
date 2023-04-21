4cb3bf88e49541e813d09e49c08187f157cc295f
Midesax <ayofavour202@gmail.com>
2023-04-21 08:31:35 -0700

    ...

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,9 +13,9 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is ", n);
-printf("%d", n % 10)
-
+printf("Last digit of %d is ", n); 
+printf("%d", n % 10);
+    
 if (n % 10 > 5)
 {
 printf(" and is greater than 5\n");
