ab14528533b7f3f805042ebcbbb7a00e3197797c
Midesax <ayofavour202@gmail.com>
2023-04-21 10:56:50 -0700

    ..

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,14 +5,15 @@
 *
 * Return: Always 0 (Success)
 */
-int main() 
+int main(void)
 {
 int i = 0;
-  
 while (i < 10) {
-printf("%d\n", i);
+printf("%d", i);
 i++;
 }
- 
+{
+printf('\n')
+}
 return (0);
 }
