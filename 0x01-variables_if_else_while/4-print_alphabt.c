f732d14a4d22dd66fed1294e480283a93e2a39e2
Midesax <ayofavour202@gmail.com>
2023-04-21 10:23:31 -0700

    ..

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+char lowercase = 'a';
+while (lowercase <= 'a');
+if (lowercase != 'e' != 'q')
+{
+purchar (lowercase);
+}
+
+return (0);
+}
