2436d165afc0392199978d9632a2b177c97b0e66
Midesax <ayofavour202@gmail.com>
2023-04-25 09:17:03 -0700

    ..

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isalpha - Checks for alphabetic character
+* @c: The character to be checked
+* Returns: 1 if c is an alphabet and 0 if otherwise
+**/
+
+int _isalpha(int c)
+{
+if ((c >= 65 && <= 90) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
+}
