7ab11a221ce6cee66674b4203ffd783b0ade514b
Midesax <ayofavour202@gmail.com>
2023-04-25 08:23:46 -0700

    '''

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,20 @@
 * Return: Always 0 (Success)
 */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void;
 {
+
 char c;
 int i = 0;
+
 while (i <= 0)
 {
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
-
+}
 _putchar('\n');
 i++;
 }
-return (0);
+
 }
