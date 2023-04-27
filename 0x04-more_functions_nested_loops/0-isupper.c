94e28881314e6b8be13743842505db0a7b47d875
Midesax <ayofavour202@gmail.com>
2023-04-27 11:58:25 -0700

    ..

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* _isupper : Checks for upper case character
+* _isupper - Checks for upper case character
 * @c: Character to be checked
-* Return: 1 if c is uppercase, 0 otherwise 
+* Return: 1 if c is uppercase, 0 otherwise
 */
 
 int _isupper(int c)
