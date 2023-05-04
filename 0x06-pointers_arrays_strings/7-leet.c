ecc6a84cda6c9746a96026da39dcc7dbc6cf00be
Midesax <ayofavour202@gmail.com>
2023-05-04 08:00:27 -0700

    ..

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,19 +6,20 @@
 /**
 * leet - encodes a string into 1337
 * @str: The string to be considered
+* @i: Integer variable for loop
+* @j: Integer variable for loop
 * Return: the encoded string
-*
 */
 
 char *leet(char *str)
 {
-int i; size_t j;
+int i, j;
 char leet_char[] = "44337711";
 char leet_letter[] = "AEOTLaeotl";
 char *encoded_str = malloc(strlen(str) + 1);
 for (i = 0; str[i] != '\0'; i++)
 {
-for (j = 0; j < strlen(leet_letter); j++)
+for (j = 0; j < 11; j++)
 {
 if (str[i] == leet_letter[j])
 {
@@ -35,4 +36,3 @@ encoded_str[i] = '\0';
 
 return encoded_str;
 }
-
