97c3dbed90e2a0f86ffdad762ec239fd10eb4edf
Midesax <ayofavour202@gmail.com>
2023-05-04 08:49:59 -0700

    ..

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,35 +1,31 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
- * @str: The string to be considered
- * @i: Integer variable for loop
- * @j: Integer variable for loop
- * Return: the encoded string
- */
-char *leet(char *str)
-{
+* leet - encodes a string into 1337
+* @str: The string to be considered
+* @i: Integer variable for loop
+* @j: Integer variable for loop
+* Return: the encoded string
+*/
+
+char *leet(char *str) {
 int i, j;
 char leet_char[] = "44337711";
 char leet_letter[] = "AEOTLaeotl";
-char s[100];
-
-for (i = 0; str[i] != '\0'; i++)
-{
-for (j = 0; j < 9; j++)
-{
-if (str[i] == leet_letter[j])
-{
-s[i] = leet_char[j];
+char *encoded_str = malloc(strlen(str) + 1);
+    
+for (i = 0; str[i] != '\0'; i++) {
+for (j = 0; j < strlen(leet_letter); j++) {
+if (str[i] == leet_letter[j]) {
+encoded_str[i] = leet_char[j];
 break;
 }
 else
 {
-s[i] = str[i];
+encoded_str[i] = str[i];
 }
 }
 }
-s[i] = '\0';
-    
-return (s);
+encoded_str[i] = '\0';
+return encoded_str;
 }
