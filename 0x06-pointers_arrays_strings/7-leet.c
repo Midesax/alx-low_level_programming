af12dc80c4a638b8209a9e3d0e91548099317df0
Midesax <ayofavour202@gmail.com>
2023-05-04 07:30:07 -0700

    ..

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* leet - encodes a string into 1337
+* @str: The string to be considered
+*Return: the encoded string
+*
+*/
+
+char *leet(char *str)
+{
+int i, j;
+char leet_char[] = "44337711";
+char leet_letter[] = "AEOTLaeotl";
+char *encoded_str = alpha(strlen(str) + 1);
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; j < strlen(leet_letter); j++)
+{
+if (str[i] == leet_letter[j])
+{
+encoded_str[i] = leet_char[j];
+break;
+}
+else
+{
+encoded_str[i] = str[i];
+}
+}
+}
+encoded_str[i] = '\0';
+
+return encoded_str;
+}
+
