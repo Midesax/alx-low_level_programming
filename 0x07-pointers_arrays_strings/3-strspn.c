9dcf1ee11e0c26d24bc91248075b14f2507dadc5
Midesax <ayofavour202@gmail.com>
2023-05-09 22:03:15 -0700

    ..

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,16 +9,19 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, n, val, get;
-val = 0;
+unsigned int i, n, value, check;
+value = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
-get = 0;
+check = 0;
 for (n = 0; accept[n] != '\0'; n++)
 {
 if (accept[n] == s[i])
 {
-val++;
+value++;
 check = 1;
 }
+}
+}
+return (n);
 }
