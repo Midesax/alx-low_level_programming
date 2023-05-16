9e1aa4c8ea22ab324c63b752d23db1841b685f83
Midesax <ayofavour202@gmail.com>
2023-05-16 06:58:35 -0700

    ..

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,4 +23,20 @@ while (s2[j])
 j++;
 l = i + j;
 s = malloc((sizeof(char) * l) +1);
+if (s == NULL)
+return (NULL);
+j = 0;
+while (k < l)
+{
+if (k <= i)
+s[k] = s1[k];
+if (k >= i)
+{
+s[k] = s2[j];
+j++;
+}
+k++;
+}
+s[k] = '\0';
+return (s);
 }
