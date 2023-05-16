74f00fc15fbe26772392a05d80ed4767169719cf
Midesax <ayofavour202@gmail.com>
2023-05-16 04:14:40 -0700

    ..

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ unsigned int i;
 char *s;
 if (size == 0)
 return (NULL);
-s = malloc(size * sixeof(char));
+s = malloc(size * sizeof(char));
 if (s == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
